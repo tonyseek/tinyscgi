f5f0a65b18ab47f079cc674e59a10c1b242f7ca0
TonySeek <tonyseek@gmail.com>
2013-02-18 02:55:29 +0800

    Added a stream based netstring parser.

---
+++ src/netstring.h
@@ -0,0 +1,9 @@
+#ifndef _TINYSCGI_NETSTRING_H
+#define _TINYSCGI_NETSTRING_H
+
+#define NETSTRING_OK              (-0x0000)
+#define NETSTRING_ERROR_FORMAT    (-0x0001)
+#define NETSTRING_ERROR_NOMEM     (-0x0002)
+#define NETSTRING_ERROR_STREAM    (-0x0003)
+
+#endif
