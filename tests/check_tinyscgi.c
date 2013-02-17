a759636a46025754fe0374744fb0edff35487ef1
TonySeek <tonyseek@gmail.com>
2013-02-18 02:56:11 +0800

    Added more unit tests for scgi header list.

--- tests/check_tinyscgi.c
+++ tests/check_tinyscgi.c
@@ -7,9 +7,50 @@ START_TEST(test_header_list)
     struct header_list *list;
 
     list = create_header_list();
-    fail_unless(list->item.name == NULL, "the list field is not clean.");
-    fail_unless(list->item.value == NULL, "the list field is not clean.");
-    fail_unless(list->next == NULL, "the next pointer is not clean.");
+
+    /* checks initial values */
+    {
+        fail_unless(list->item.name == NULL);
+        fail_unless(list->item.value == NULL);
+        fail_unless(list->next == NULL);
+    }
+
+    /* checks appending */
+    {
+        struct header headers[] =
+        {
+            {"CONTENT_LENGTH", "0"},
+            {"SCGI", "1"},
+            {"REQUEST_METHOD", "GET"},
+        };
+        size_t i;
+        size_t len = sizeof(headers) / sizeof(struct header);
+
+        for (i=0; i<len; i++)
+            append_header_list(list, &headers[i]);
+    }
+    {
+        /* the header creating context has been unwound. */
+        struct header_list *l;
+        /* 0 */
+        l = list;
+        fail_unless(l->item.name == NULL);
+        fail_unless(l->item.value == NULL);
+        /* 1 */
+        l = l->next;
+        fail_unless(l->item.name == "CONTENT_LENGTH");
+        fail_unless(l->item.value == "0");
+        /* 2 */
+        l = l->next;
+        fail_unless(l->item.name == "SCGI");
+        fail_unless(l->item.value == "1");
+        /* 3 */
+        l = l->next;
+        fail_unless(l->item.name == "REQUEST_METHOD");
+        fail_unless(l->item.value == "GET");
+        /* last */
+        fail_unless(l->next == NULL);
+    }
 }
 END_TEST
 
