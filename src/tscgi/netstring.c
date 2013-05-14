e5d701e44d85cf0a02b16f28f1fa8948a227014e
TonySeek <tonyseek@gmail.com>
2013-05-14 12:03:17 +0800

    important: use tscgi/buffer.c instead of stdio.h stream.

--- src/tscgi/netstring.c
+++ src/tscgi/netstring.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "errors.h"
 #include "netstring.h"
+#include "buffer.h"
 
-int parse_netstring(FILE *stream, char **buffer, size_t *length)
+int parse_netstring(struct buffer *stream, char **buffer, size_t *length)
 {
     char *buf;
     size_t len;
+    int rv, nscanf;
 
-    if (fscanf(stream, "%9lu", &len) < 1)
+    rv = sscanf(buffer_current(stream), "%9lu%n", &len, &nscanf);
+    if (rv != 1 || nscanf <= 0)
         return NETSTRING_ERROR_STREAM;
+    else
+        buffer_forward(stream, nscanf);
 
-    if (getc(stream) != ':')
+    if (buffer_getc(stream) != ':')
         return NETSTRING_ERROR_FORMAT;
 
     buf = (char *) malloc(len + 1);
     if (!buf)
         return NETSTRING_ERROR_NOMEM;
 
-    if (fread(buf, sizeof(char), len, stream) < len)
+    if (buffer_len(stream) - buffer_pos(stream) < len + 1)
+    {
+        free(buf);
         return NETSTRING_ERROR_STREAM;
+    }
+    else
+    {
+        memcpy(buf, buffer_current(stream), len);
+        buffer_forward(stream, len);
+    }
 
-    if (getc(stream) != ',')
+    if (buffer_getc(stream) != ',')
+    {
+        free(buf);
         return NETSTRING_ERROR_FORMAT;
+    }
 
     /* out parameter */
     if (buffer)
--- src/tscgi/request.c
+++ src/tscgi/request.c
@@ -5,12 +5,14 @@
 #include "netstring.h"
 #include "header.h"
 #include "request.h"
+#include "buffer.h"
 
-int parse_request(FILE *stream, struct request *request, size_t body_max_len)
+int parse_request(struct buffer *stream, struct request *request,
+                  size_t body_max_len)
 {
     int ret;
     char *headers_buffer, *body_buffer;
-    size_t headers_len, body_len, read_len;
+    size_t headers_len, body_len;
     struct header_list *headers;
 
     /* parse netstring */
@@ -33,21 +35,19 @@ int parse_request(FILE *stream, struct request *request, size_t body_max_len)
     /* read body */
     if (body_len > body_max_len)
         return REQUEST_ERROR_BODY_TOO_MAX;
-    if (body_len)
+    if (body_len > 0)
     {
-        body_buffer = (char *) malloc(body_len);
-        memset(body_buffer, 0, body_len);
-        read_len = fread(body_buffer, sizeof(char), body_len, stream);
-        if (!read_len)
+        if ((buffer_len(stream) - buffer_pos(stream)) < body_len)
             return REQUEST_ERROR_BROKEN_PIPE;
-        else
-            /* be safe: strange content may be read out buffer size in some
-             * operation system, such as Mac OSX. */
-            body_buffer[read_len] = '\0';
+        body_buffer = (char *) malloc(sizeof(char) * body_len);
+        memcpy(body_buffer, buffer_current(stream), body_len);
+        /* be safe: strange content may be read out buffer size in some
+         * operation system, such as Mac OSX. */
+        body_buffer[body_len] = '\0';
     }
     else
     {
-        body_buffer = "";
+        body_buffer = EMPTY_BUFFER;
     }
 
     /* save public attributes */
