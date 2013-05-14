641208f099f7a84420a6fdfb79ba233b4fc851c6
TonySeek <tonyseek@gmail.com>
2013-05-14 15:43:21 +0800

    modified tests to use new ck_assert_* instead of deprecated fail_*.

--- tests/test_buffer.c
+++ tests/test_buffer.c
@@ -19,40 +19,40 @@ START_TEST(test_buffer)
             buffer_init(&buf, str, 10);
 
             /* testing initialize */
-            fail_unless(buffer_current(&buf) == str);
-            fail_unless(buffer_len(&buf) == 10);
-            fail_unless(buffer_pos(&buf) == 0);
+            ck_assert(buffer_current(&buf) == str);
+            ck_assert(buffer_len(&buf) == 10);
+            ck_assert(buffer_pos(&buf) == 0);
             ck_assert_str_eq(buffer_current(&buf), "aaaaaaaaaa");
 
             /* testing forward */
             buffer_forward(&buf, 2);
-            fail_unless(buffer_pos(&buf) == 2);
+            ck_assert(buffer_pos(&buf) == 2);
             ck_assert_str_eq(buffer_current(&buf), "aaaaaaaa");
 
             buffer_forward(&buf, 4);
-            fail_unless(buffer_pos(&buf) == 6);
+            ck_assert(buffer_pos(&buf) == 6);
             ck_assert_str_eq(buffer_current(&buf), "aaaa");
 
             buffer_forward(&buf, 2);
-            fail_unless(buffer_pos(&buf) == 8);
+            ck_assert(buffer_pos(&buf) == 8);
             ck_assert_str_eq(buffer_current(&buf), "aa");
 
             /* testing getc */
-            fail_unless(buffer_getc(&buf) == 'a');
-            fail_unless(buffer_pos(&buf) == 9);
+            ck_assert(buffer_getc(&buf) == 'a');
+            ck_assert(buffer_pos(&buf) == 9);
             ck_assert_str_eq(buffer_current(&buf), "a");
 
             /* testing overflow */
             buffer_forward(&buf, 1);
-            fail_unless(buffer_pos(&buf) == -1);
+            ck_assert(buffer_pos(&buf) == -1);
             ck_assert_str_eq(buffer_current(&buf), "");
 
             buffer_forward(&buf, 0);
-            fail_unless(buffer_pos(&buf) == -1);
+            ck_assert(buffer_pos(&buf) == -1);
             ck_assert_str_eq(buffer_current(&buf), "");
 
             buffer_forward(&buf, 15);
-            fail_unless(buffer_pos(&buf) == -1);
+            ck_assert(buffer_pos(&buf) == -1);
             ck_assert_str_eq(buffer_current(&buf), "");
         }
     }
