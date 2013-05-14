9887226c718ae8edff8ddb5d3607e065e01ce7ae
TonySeek <tonyseek@gmail.com>
2013-05-14 10:15:26 +0800

    a little of modified to improve source.

--- src/tscgi/buffer.c
+++ src/tscgi/buffer.c
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include "buffer.h"
 
-struct buffer
-{
-    void *ptr;
-    size_t len;
-    int pos;
-};
-
 void buffer_init(struct buffer *buffer, void *ptr, size_t len)
 {
     buffer->ptr = ptr;
@@ -20,9 +13,10 @@ inline size_t buffer_pos(struct buffer *buffer)
     return buffer->pos;
 }
 
-inline size_t buffer_seek(struct buffer *buffer, size_t offset)
+inline size_t buffer_forward(struct buffer *buffer, size_t offset)
 {
-    buffer->pos += offset;
+    if (offset && (buffer->pos >= 0))
+        buffer->pos += offset;
     if (buffer->pos >= buffer->len)
         buffer->pos = -1;  /* overflow */
     return buffer->pos;
@@ -30,8 +24,5 @@ inline size_t buffer_seek(struct buffer *buffer, size_t offset)
 
 inline void *buffer_current(struct buffer *buffer)
 {
-    if (buffer->pos >= 0)
-        return buffer->ptr + buffer->pos;
-    else
-        return '\0';
+    return (buffer->pos >= 0) ? (buffer->ptr + buffer->pos) : EMPTY_BUFFER;
 }
--- src/tscgi/buffer.h
+++ src/tscgi/buffer.h
@@ -1,11 +1,18 @@
 #ifndef _TINYSCGI_BUFFER_H
 #define _TINYSCGI_BUFFER_H
 
-struct buffer;
+#define EMPTY_BUFFER ("\0")
+
+struct buffer
+{
+    void *ptr;
+    size_t len;
+    int pos;
+};
 
 extern void buffer_init(struct buffer *buffer, void *ptr, size_t len);
 extern inline size_t buffer_pos(struct buffer *buffer);
-extern inline size_t buffer_seek(struct buffer *buffer, size_t offset);
+extern inline size_t buffer_forward(struct buffer *buffer, size_t offset);
 extern inline void *buffer_current(struct buffer *buffer);
 
 #endif
